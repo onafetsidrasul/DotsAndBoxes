53cfe1f934f89477282289da8fa740548c1238e4
simdeistud <simone.deiana@studenti.units.it>
2024-02-06 17:43:16 +0100

    Fixed criterion to check if line is valid inside the constructor.

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,7 +1,7 @@
 public record Line(Player player, int x1, int y1, int x2, int y2) {
     public Line {
-        if (Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2) != 1) {
-            throw new IllegalArgumentException("Invalid line");
+        if (Math.pow((x2 - x1), 0.5) + Math.pow((y2 - y1), 0.5) > 1) {
+            throw new IllegalArgumentException("Illegal line");
         }
     }
 
