2d76e696e152105047dcb3435862b8626fc261a9
simdeistud <simone.deiana@studenti.units.it>
2024-02-07 16:11:21 +0100

    Added method to create line from an already existing line but overwriting its color. This is useful to the Game class when making a new move

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -3,4 +3,8 @@ public record Line(Color color, int x1, int y1, int x2, int y2) {
         this(null, x1, y1, x2, y2);
     }
 
+    public Line(Color color, Line line) {
+        // overwrites the color of the supplied line
+        this(color, line.x1(), line.y1(), line.x2(), line.y2());
+    }
 }
