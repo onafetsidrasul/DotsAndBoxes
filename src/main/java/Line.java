2456096376827e694d38e5fd7e9c1ac194a96f30
simdeistud <simone.deiana@studenti.units.it>
2024-02-07 13:35:41 +0100

    Simplified the Line record. Removed method and removed check on the constructor. It is the Game or the Board's job to check if a line is valid before adding it, we should not limit the type of line immediately in the constructor.

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,16 +1,2 @@
-public record Line(Player player, int x1, int y1, int x2, int y2) {
-    public Line {
-        if (Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2) != 1) {
-            throw new IllegalArgumentException("Illegal line");
-        }
-    }
-
-    public Line(int x1, int y1, int x2, int y2) {
-        this(null, x1, y1, x2, y2);
-    }
-
-    public boolean equalsIgnoringPlayer(Line other) {
-        return (this.x1 == other.x1 && this.y1 == other.y1 && this.x2 == other.x2 && this.y2 == other.y2) ||
-                (this.x1 == other.x2 && this.y1 == other.y2 && this.x2 == other.x1 && this.y2 == other.y1);
-    }
+public record Line(Color color, int x1, int y1, int x2, int y2) {
 }
