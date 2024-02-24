21909504b4193f7bd22125f91cdbf4b3260a52f4
simdeistud <simone.deiana@studenti.units.it>
2024-02-24 17:57:12 +0100

    Removed temporary workaround implemented to avoid refactoring

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -2,12 +2,6 @@ public record Line(Color color, Point p1, Point p2) {
     public Line(int x1, int y1, int x2, int y2) {
         this(null, new Point(x1, y1), new Point(x2, y2));
     }
-
-    public int x1(){return p1.x();}
-    public int y1(){return p1.y();}
-    public int x2(){return p2.x();}
-    public int y2(){return p2.y();}
-
     public Line(Color color, Line line) {
         // overwrites the color of the supplied line
         this(color, line.p1(), line.p2());
