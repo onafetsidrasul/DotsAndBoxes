c6d1fec860b611f23086a83fb8528ec24f728612
simdeistud <simone.deiana@studenti.units.it>
2024-03-09 13:15:30 +0100

    Added documentation

--- src/main/java/it/units/sdm/dotsandboxes/core/Point.java
+++ src/main/java/it/units/sdm/dotsandboxes/core/Point.java
@@ -1,4 +1,10 @@
 package it.units.sdm.dotsandboxes.core;
 
+/**
+ * Record that models a point on a Dots and Boxes board.
+ *
+ * @param x X coordinate of the point
+ * @param y Y coordinate of the point
+ */
 public record Point(int x, int y) {
 }
