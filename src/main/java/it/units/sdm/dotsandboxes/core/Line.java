67405f7a487d50a5d878103b19ec1c39e27e8489
simdeistud <simone.deiana@studenti.units.it>
2024-04-01 21:45:04 +0200

    Added methods to get the length of a line, calculate its normalized version and have a better toString representation

--- src/main/java/it/units/sdm/dotsandboxes/core/Line.java
+++ src/main/java/it/units/sdm/dotsandboxes/core/Line.java
@@ -1,16 +1,19 @@
 package it.units.sdm.dotsandboxes.core;
 
 /**
- *
  * Record that models a line on a Dots and Boxes board.
  * Lines do not formally have a direction as it's not required by the game,
  * although it can be inferred from the implicit order of the endpoints.
  *
  * @param color Color of the line
- * @param p1 First endpoint of the line
- * @param p2 Second endpoint of the line
+ * @param p1    First endpoint of the line
+ * @param p2    Second endpoint of the line
  */
-public record Line(Color color, Point p1, Point p2) {
+public record Line(
+        Color color,
+        Point p1,
+        Point p2
+) {
     public Line(int x1, int y1, int x2, int y2) {
         this(null, new Point(x1, y1), new Point(x2, y2));
     }
@@ -20,11 +23,31 @@ public record Line(Color color, Point p1, Point p2) {
     }
 
     /**
-     * Returns a line with the same endpoint as the original one but a new color
+     * Creates a line with the same endpoint as the original one but a new color
+     *
      * @param color Color of the new line
-     * @param line Line to color
+     * @param line  Line to color
      */
     public Line(Color color, Line line) {
         this(color, line.p1(), line.p2());
+    }
+
+    public double length() {
+        return Math.pow(Math.pow(p2.x() - p1.x(), 2) + Math.pow(p2.y() - p1.y(), 2), 0.5);
+    }
+
+    /**
+     * Returns a normalized version of the line
+     *
+     */
+    public static Line normalize(Line line) {
+        if (line.p1().x() > line.p2().x() || line.p1().y() > line.p2().y()) {
+            return new Line(line.color(), new Point(line.p2().x(), line.p2().y()), new Point(line.p1().x(), line.p1().y()));
+        }
+        return line;
+    }
+
+    public String toString(){
+        return p1.toString() + ", " + p2.toString();
     }
 }
