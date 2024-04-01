66ffed9a1a818d29c0c778508686d635497f5a02
simdeistud <simone.deiana@studenti.units.it>
2024-04-01 21:45:28 +0200

    Refactoring following class changes

--- src/test/java/it/units/sdm/dotsandboxes/Tests.java
+++ src/test/java/it/units/sdm/dotsandboxes/Tests.java
@@ -13,7 +13,7 @@ public class Tests {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0,0,0,2)));
+        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0, 0, 0, 2)));
     }
 
     @Test
@@ -21,56 +21,58 @@ public class Tests {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0,0,1,2)));
+        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0, 0, 1, 2)));
     }
 
     @Test
-    void LinesThatStartOutOfBoundsAreNotAllowed(){
+    void LinesThatStartOutOfBoundsAreNotAllowed() {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(6,5,5,5)));
+        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(6, 5, 5, 5)));
     }
 
     @Test
-    void LinesThatEndOutOfBoundsAreNotAllowed(){
+    void LinesThatEndOutOfBoundsAreNotAllowed() {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0,0,-1,0)));
+        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0, 0, -1, 0)));
     }
 
     @Test
-    void OverwritingLinesIsNotAllowed(){
+    void OverwritingLinesIsNotAllowed() {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        testGame.makeNextMove(new Line( 0, 0, 1, 0));
-        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0,0,1,0)));
+        testGame.makeNextMove(new Line(0, 0, 1, 0));
+        assertThrows(Exception.class, () -> testGame.makeNextMove(new Line(0, 0, 1, 0)));
     }
 
     @Test
     void upperLeftBoxIsCompleted() {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
-        Game testGame = new Game(player1, player2,5,5);
+        Game testGame = new Game(player1, player2, 5, 5);
         testGame.makeNextMove(new Line(0, 0, 1, 0));
         testGame.makeNextMove(new Line(0, 1, 1, 1));
         testGame.makeNextMove(new Line(0, 0, 0, 1));
         testGame.makeNextMove(new Line(1, 0, 1, 1));
         assertTrue(testGame.getGameBoard().isBoxCompleted(new Point(0, 0)));
     }
+
     @Test
-    void player1StartsFirst(){
-        Game testGame = new Game(new Player("A", Color.RED), new Player("B", Color.BLUE),5,5);
+    void player1StartsFirst() {
+        Game testGame = new Game(new Player("A", Color.RED), new Player("B", Color.BLUE), 5, 5);
         assertEquals("A", testGame.getCurrentPlayer().getName());
     }
+
     @Test
-    void playersCorrectlySwitch(){
+    void playersCorrectlySwitch() {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
-        Game testGame = new Game(player1, player2,5,5);
-        testGame.makeNextMove(new Line(0,0,0,1));
+        Game testGame = new Game(player1, player2, 5, 5);
+        testGame.makeNextMove(new Line(0, 0, 0, 1));
         assertEquals("B", testGame.getCurrentPlayer().getName());
     }
 
@@ -78,65 +80,65 @@ public class Tests {
     void TwoBoxesCompletedByTwoPlayers() {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
-        Game testGame = new Game(player1, player2,5,5);
-        testGame.makeNextMove(new Line( 0, 0, 1, 0));
-        testGame.makeNextMove(new Line( 0, 1, 1, 1));
-        testGame.makeNextMove(new Line( 0, 0, 0, 1));
-        testGame.makeNextMove(new Line( 1, 0, 1, 1));
+        Game testGame = new Game(player1, player2, 5, 5);
+        testGame.makeNextMove(new Line(0, 0, 1, 0));
+        testGame.makeNextMove(new Line(0, 1, 1, 1));
+        testGame.makeNextMove(new Line(0, 0, 0, 1));
+        testGame.makeNextMove(new Line(1, 0, 1, 1));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 0, 1, 0, 2));
-        testGame.makeNextMove(new Line( 0, 2, 1, 2));
-        testGame.makeNextMove(new Line( 1, 1, 1, 2));
+        testGame.makeNextMove(new Line(0, 1, 0, 2));
+        testGame.makeNextMove(new Line(0, 2, 1, 2));
+        testGame.makeNextMove(new Line(1, 1, 1, 2));
         testGame.updateScore();
         assertEquals(1, player1.getScore());
         assertEquals(1, player2.getScore());
     }
 
     @Test
-    void boardFullMeansGameHasEnded(){
+    void boardFullMeansGameHasEnded() {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        testGame.makeNextMove(new Line( 0, 0, 1, 0));
-        testGame.makeNextMove(new Line( 0, 1, 1, 1));
-        testGame.makeNextMove(new Line( 0, 0, 0, 1));
-        testGame.makeNextMove(new Line( 1, 0, 1, 1));
-        testGame.makeNextMove(new Line( 0, 1, 0, 2));
-        testGame.makeNextMove(new Line( 0, 2, 1, 2));
-        testGame.makeNextMove(new Line( 1, 1, 1, 2));
-        testGame.makeNextMove(new Line( 0, 2, 0, 3));
-        testGame.makeNextMove(new Line( 0, 3, 1, 3));
-        testGame.makeNextMove(new Line( 1, 3, 1, 2));
-        testGame.makeNextMove(new Line( 0, 3, 0, 4));
-        testGame.makeNextMove(new Line( 0, 4, 1, 4));
-        testGame.makeNextMove(new Line( 1, 4, 1, 3));
-        testGame.makeNextMove(new Line( 1, 4, 2, 4));
-        testGame.makeNextMove(new Line( 2, 4, 2, 3));
-        testGame.makeNextMove(new Line( 1, 3, 2, 3));
-        testGame.makeNextMove(new Line( 2, 3, 2, 2));
-        testGame.makeNextMove(new Line( 1, 2, 2, 2));
-        testGame.makeNextMove(new Line( 2, 1, 2, 2));
-        testGame.makeNextMove(new Line( 2, 1, 1, 1));
-        testGame.makeNextMove(new Line( 2, 1, 2, 0));
-        testGame.makeNextMove(new Line( 2, 0, 1, 0));
-        testGame.makeNextMove(new Line( 2, 0, 3, 0));
-        testGame.makeNextMove(new Line( 3, 0, 3, 1));
-        testGame.makeNextMove(new Line( 3, 1, 2, 1));
-        testGame.makeNextMove(new Line( 3, 1, 3, 2));
-        testGame.makeNextMove(new Line( 3, 2, 2, 2));
-        testGame.makeNextMove(new Line( 3, 2, 3, 3));
-        testGame.makeNextMove(new Line( 3, 3, 2, 3));
-        testGame.makeNextMove(new Line( 3, 4, 2, 4));
-        testGame.makeNextMove(new Line( 3, 3, 3, 4));
-        testGame.makeNextMove(new Line( 3, 4, 4, 4));
-        testGame.makeNextMove(new Line( 4, 4, 4, 3));
-        testGame.makeNextMove(new Line( 4, 3, 3, 3));
-        testGame.makeNextMove(new Line( 4, 3, 4, 2));
-        testGame.makeNextMove(new Line( 4, 2, 3, 2));
-        testGame.makeNextMove(new Line( 4, 1, 3, 1));
-        testGame.makeNextMove(new Line( 4, 1, 4, 2));
-        testGame.makeNextMove(new Line( 4, 1, 4, 0));
-        testGame.makeNextMove(new Line( 4, 0, 3, 0));
+        testGame.makeNextMove(new Line(0, 0, 1, 0));
+        testGame.makeNextMove(new Line(0, 1, 1, 1));
+        testGame.makeNextMove(new Line(0, 0, 0, 1));
+        testGame.makeNextMove(new Line(1, 0, 1, 1));
+        testGame.makeNextMove(new Line(0, 1, 0, 2));
+        testGame.makeNextMove(new Line(0, 2, 1, 2));
+        testGame.makeNextMove(new Line(1, 1, 1, 2));
+        testGame.makeNextMove(new Line(0, 2, 0, 3));
+        testGame.makeNextMove(new Line(0, 3, 1, 3));
+        testGame.makeNextMove(new Line(1, 3, 1, 2));
+        testGame.makeNextMove(new Line(0, 3, 0, 4));
+        testGame.makeNextMove(new Line(0, 4, 1, 4));
+        testGame.makeNextMove(new Line(1, 4, 1, 3));
+        testGame.makeNextMove(new Line(1, 4, 2, 4));
+        testGame.makeNextMove(new Line(2, 4, 2, 3));
+        testGame.makeNextMove(new Line(1, 3, 2, 3));
+        testGame.makeNextMove(new Line(2, 3, 2, 2));
+        testGame.makeNextMove(new Line(1, 2, 2, 2));
+        testGame.makeNextMove(new Line(2, 1, 2, 2));
+        testGame.makeNextMove(new Line(2, 1, 1, 1));
+        testGame.makeNextMove(new Line(2, 1, 2, 0));
+        testGame.makeNextMove(new Line(2, 0, 1, 0));
+        testGame.makeNextMove(new Line(2, 0, 3, 0));
+        testGame.makeNextMove(new Line(3, 0, 3, 1));
+        testGame.makeNextMove(new Line(3, 1, 2, 1));
+        testGame.makeNextMove(new Line(3, 1, 3, 2));
+        testGame.makeNextMove(new Line(3, 2, 2, 2));
+        testGame.makeNextMove(new Line(3, 2, 3, 3));
+        testGame.makeNextMove(new Line(3, 3, 2, 3));
+        testGame.makeNextMove(new Line(3, 4, 2, 4));
+        testGame.makeNextMove(new Line(3, 3, 3, 4));
+        testGame.makeNextMove(new Line(3, 4, 4, 4));
+        testGame.makeNextMove(new Line(4, 4, 4, 3));
+        testGame.makeNextMove(new Line(4, 3, 3, 3));
+        testGame.makeNextMove(new Line(4, 3, 4, 2));
+        testGame.makeNextMove(new Line(4, 2, 3, 2));
+        testGame.makeNextMove(new Line(4, 1, 3, 1));
+        testGame.makeNextMove(new Line(4, 1, 4, 2));
+        testGame.makeNextMove(new Line(4, 1, 4, 0));
+        testGame.makeNextMove(new Line(4, 0, 3, 0));
         assertTrue(testGame.hasEnded());
     }
 
@@ -145,46 +147,46 @@ public class Tests {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        testGame.makeNextMove(new Line( 0, 0, 1, 0));
-        testGame.makeNextMove(new Line( 0, 1, 1, 1));
-        testGame.makeNextMove(new Line( 0, 0, 0, 1));
-        testGame.makeNextMove(new Line( 1, 0, 1, 1));
-        testGame.makeNextMove(new Line( 0, 1, 0, 2));
-        testGame.makeNextMove(new Line( 0, 2, 1, 2));
-        testGame.makeNextMove(new Line( 1, 1, 1, 2));
-        testGame.makeNextMove(new Line( 0, 2, 0, 3));
-        testGame.makeNextMove(new Line( 0, 3, 1, 3));
-        testGame.makeNextMove(new Line( 1, 3, 1, 2));
-        testGame.makeNextMove(new Line( 0, 3, 0, 4));
-        testGame.makeNextMove(new Line( 0, 4, 1, 4));
-        testGame.makeNextMove(new Line( 1, 4, 1, 3));
-        testGame.makeNextMove(new Line( 1, 4, 2, 4));
-        testGame.makeNextMove(new Line( 2, 4, 2, 3));
-        testGame.makeNextMove(new Line( 1, 3, 2, 3));
-        testGame.makeNextMove(new Line( 2, 3, 2, 2));
-        testGame.makeNextMove(new Line( 1, 2, 2, 2));
-        testGame.makeNextMove(new Line( 2, 1, 2, 2));
-        testGame.makeNextMove(new Line( 2, 1, 1, 1));
-        testGame.makeNextMove(new Line( 2, 1, 2, 0));
-        testGame.makeNextMove(new Line( 2, 0, 1, 0));
-        testGame.makeNextMove(new Line( 2, 0, 3, 0));
-        testGame.makeNextMove(new Line( 3, 0, 3, 1));
-        testGame.makeNextMove(new Line( 3, 1, 2, 1));
-        testGame.makeNextMove(new Line( 3, 1, 3, 2));
-        testGame.makeNextMove(new Line( 3, 2, 2, 2));
-        testGame.makeNextMove(new Line( 3, 2, 3, 3));
-        testGame.makeNextMove(new Line( 3, 3, 2, 3));
-        testGame.makeNextMove(new Line( 3, 4, 2, 4));
-        testGame.makeNextMove(new Line( 3, 3, 3, 4));
-        testGame.makeNextMove(new Line( 3, 4, 4, 4));
-        testGame.makeNextMove(new Line( 4, 4, 4, 3));
-        testGame.makeNextMove(new Line( 4, 3, 3, 3));
-        testGame.makeNextMove(new Line( 4, 3, 4, 2));
-        testGame.makeNextMove(new Line( 4, 2, 3, 2));
-        testGame.makeNextMove(new Line( 4, 1, 3, 1));
-        testGame.makeNextMove(new Line( 4, 1, 4, 2));
-        testGame.makeNextMove(new Line( 4, 1, 4, 0));
-        testGame.makeNextMove(new Line( 4, 0, 3, 0));
+        testGame.makeNextMove(new Line(0, 0, 1, 0));
+        testGame.makeNextMove(new Line(0, 1, 1, 1));
+        testGame.makeNextMove(new Line(0, 0, 0, 1));
+        testGame.makeNextMove(new Line(1, 0, 1, 1));
+        testGame.makeNextMove(new Line(0, 1, 0, 2));
+        testGame.makeNextMove(new Line(0, 2, 1, 2));
+        testGame.makeNextMove(new Line(1, 1, 1, 2));
+        testGame.makeNextMove(new Line(0, 2, 0, 3));
+        testGame.makeNextMove(new Line(0, 3, 1, 3));
+        testGame.makeNextMove(new Line(1, 3, 1, 2));
+        testGame.makeNextMove(new Line(0, 3, 0, 4));
+        testGame.makeNextMove(new Line(0, 4, 1, 4));
+        testGame.makeNextMove(new Line(1, 4, 1, 3));
+        testGame.makeNextMove(new Line(1, 4, 2, 4));
+        testGame.makeNextMove(new Line(2, 4, 2, 3));
+        testGame.makeNextMove(new Line(1, 3, 2, 3));
+        testGame.makeNextMove(new Line(2, 3, 2, 2));
+        testGame.makeNextMove(new Line(1, 2, 2, 2));
+        testGame.makeNextMove(new Line(2, 1, 2, 2));
+        testGame.makeNextMove(new Line(2, 1, 1, 1));
+        testGame.makeNextMove(new Line(2, 1, 2, 0));
+        testGame.makeNextMove(new Line(2, 0, 1, 0));
+        testGame.makeNextMove(new Line(2, 0, 3, 0));
+        testGame.makeNextMove(new Line(3, 0, 3, 1));
+        testGame.makeNextMove(new Line(3, 1, 2, 1));
+        testGame.makeNextMove(new Line(3, 1, 3, 2));
+        testGame.makeNextMove(new Line(3, 2, 2, 2));
+        testGame.makeNextMove(new Line(3, 2, 3, 3));
+        testGame.makeNextMove(new Line(3, 3, 2, 3));
+        testGame.makeNextMove(new Line(3, 4, 2, 4));
+        testGame.makeNextMove(new Line(3, 3, 3, 4));
+        testGame.makeNextMove(new Line(3, 4, 4, 4));
+        testGame.makeNextMove(new Line(4, 4, 4, 3));
+        testGame.makeNextMove(new Line(4, 3, 3, 3));
+        testGame.makeNextMove(new Line(4, 3, 4, 2));
+        testGame.makeNextMove(new Line(4, 2, 3, 2));
+        testGame.makeNextMove(new Line(4, 1, 3, 1));
+        testGame.makeNextMove(new Line(4, 1, 4, 2));
+        testGame.makeNextMove(new Line(4, 1, 4, 0));
+        testGame.makeNextMove(new Line(4, 0, 3, 0));
         assertEquals(List.of(player1, player2), GameSession.winner(List.of(player1, player2)));
     }
 
@@ -193,75 +195,71 @@ public class Tests {
         Player player1 = new Player("A", Color.RED);
         Player player2 = new Player("B", Color.BLUE);
         Game testGame = new Game(player1, player2, 5, 5);
-        testGame.makeNextMove(new Line( 0, 0, 1, 0));
-        testGame.makeNextMove(new Line( 0, 1, 1, 1));
-        testGame.makeNextMove(new Line( 0, 0, 0, 1));
-        testGame.makeNextMove(new Line( 1, 0, 1, 1));
+        testGame.makeNextMove(new Line(0, 0, 1, 0));
+        testGame.makeNextMove(new Line(0, 1, 1, 1));
+        testGame.makeNextMove(new Line(0, 0, 0, 1));
+        testGame.makeNextMove(new Line(1, 0, 1, 1));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 0, 1, 0, 2));
-        testGame.makeNextMove(new Line( 0, 2, 1, 2));
-        testGame.makeNextMove(new Line( 1, 1, 1, 2));
+        testGame.makeNextMove(new Line(0, 1, 0, 2));
+        testGame.makeNextMove(new Line(0, 2, 1, 2));
+        testGame.makeNextMove(new Line(1, 1, 1, 2));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 0, 2, 0, 3));
-        testGame.makeNextMove(new Line( 0, 3, 1, 3));
-        testGame.makeNextMove(new Line( 1, 3, 1, 2));
+        testGame.makeNextMove(new Line(0, 2, 0, 3));
+        testGame.makeNextMove(new Line(0, 3, 1, 3));
+        testGame.makeNextMove(new Line(1, 3, 1, 2));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 0, 3, 0, 4));
-        testGame.makeNextMove(new Line( 0, 4, 1, 4));
-        testGame.makeNextMove(new Line( 1, 4, 1, 3));
+        testGame.makeNextMove(new Line(0, 3, 0, 4));
+        testGame.makeNextMove(new Line(0, 4, 1, 4));
+        testGame.makeNextMove(new Line(1, 4, 1, 3));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 1, 4, 2, 4));
-        testGame.makeNextMove(new Line( 2, 4, 2, 3));
-        testGame.makeNextMove(new Line( 1, 3, 2, 3));
+        testGame.makeNextMove(new Line(1, 4, 2, 4));
+        testGame.makeNextMove(new Line(2, 4, 2, 3));
+        testGame.makeNextMove(new Line(1, 3, 2, 3));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 2, 3, 2, 2));
-        testGame.makeNextMove(new Line( 1, 2, 2, 2));
+        testGame.makeNextMove(new Line(2, 3, 2, 2));
+        testGame.makeNextMove(new Line(1, 2, 2, 2));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 2, 1, 2, 2));
-        testGame.makeNextMove(new Line( 2, 1, 1, 1));
+        testGame.makeNextMove(new Line(2, 1, 2, 2));
+        testGame.makeNextMove(new Line(2, 1, 1, 1));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 2, 1, 2, 0));
-        testGame.makeNextMove(new Line( 2, 0, 1, 0));
+        testGame.makeNextMove(new Line(2, 1, 2, 0));
+        testGame.makeNextMove(new Line(2, 0, 1, 0));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 2, 0, 3, 0));
-        testGame.makeNextMove(new Line( 3, 0, 3, 1));
-        testGame.makeNextMove(new Line( 3, 1, 2, 1));
+        testGame.makeNextMove(new Line(2, 0, 3, 0));
+        testGame.makeNextMove(new Line(3, 0, 3, 1));
+        testGame.makeNextMove(new Line(3, 1, 2, 1));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 3, 1, 3, 2));
-        testGame.makeNextMove(new Line( 3, 2, 2, 2));
+        testGame.makeNextMove(new Line(3, 1, 3, 2));
+        testGame.makeNextMove(new Line(3, 2, 2, 2));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 3, 2, 3, 3));
-        testGame.makeNextMove(new Line( 3, 3, 2, 3));
+        testGame.makeNextMove(new Line(3, 2, 3, 3));
+        testGame.makeNextMove(new Line(3, 3, 2, 3));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 3, 4, 2, 4));
-        testGame.makeNextMove(new Line( 3, 3, 3, 4));
+        testGame.makeNextMove(new Line(3, 4, 2, 4));
+        testGame.makeNextMove(new Line(3, 3, 3, 4));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 3, 4, 4, 4));
-        testGame.makeNextMove(new Line( 4, 0, 3, 0));
-        testGame.makeNextMove(new Line( 4, 1, 4, 0));
-        testGame.makeNextMove(new Line( 4, 1, 3, 1));
+        testGame.makeNextMove(new Line(3, 4, 4, 4));
+        testGame.makeNextMove(new Line(4, 0, 3, 0));
+        testGame.makeNextMove(new Line(4, 1, 4, 0));
+        testGame.makeNextMove(new Line(4, 1, 3, 1));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 4, 1, 4, 2));
-        testGame.makeNextMove(new Line( 4, 2, 3, 2));
+        testGame.makeNextMove(new Line(4, 1, 4, 2));
+        testGame.makeNextMove(new Line(4, 2, 3, 2));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 4, 3, 4, 2));
-        testGame.makeNextMove(new Line( 4, 3, 3, 3));
+        testGame.makeNextMove(new Line(4, 3, 4, 2));
+        testGame.makeNextMove(new Line(4, 3, 3, 3));
         testGame.updateScore();
-        testGame.makeNextMove(new Line( 4, 4, 4, 3));
+        testGame.makeNextMove(new Line(4, 4, 4, 3));
         testGame.updateScore();
         assertEquals(List.of(player1), GameSession.winner(List.of(player1, player2)));
     }
 
     @Test
     void equals() {
-        Line a = new Line(0, 1, 0,0);
-        Line b = new Line(0, 0, 0,1);
-        if (Board.isNotNormalized(a)){
-            a=Board.normalizer(a);
-        }
-        if (Board.isNotNormalized(b)){
-            b=Board.normalizer(b);
-        }
-        assertEquals(a,b);
+        Line a = new Line(0, 1, 0, 0);
+        Line b = new Line(0, 0, 0, 1);
+        a = Line.normalize(a);
+        b = Line.normalize(b);
+        assertEquals(a, b);
     }
 }
