82333ce1be7b39da41df61883df9e9db172110ab
simdeistud <simone.deiana@studenti.units.it>
2024-04-01 21:43:42 +0200

    Turned into a class to allow for better constructor. General refactoring.

--- src/main/java/it/units/sdm/dotsandboxes/core/Move.java
+++ src/main/java/it/units/sdm/dotsandboxes/core/Move.java
@@ -1,4 +1,29 @@
 package it.units.sdm.dotsandboxes.core;
 
-public record Move(Player player, Integer lineHash) {
+import java.util.Objects;
+
+public final class Move {
+    private final Player player;
+    private final Integer colorlessNormalizedLineHash;
+
+    public Move(Player player, Line line) {
+        this.player = player;
+        colorlessNormalizedLineHash = new Line(null, Line.normalize(line)).hashCode();
+    }
+
+    public Player player() {
+        return player;
+    }
+
+    public Integer colorlessNormalizedLineHash() {
+        return colorlessNormalizedLineHash;
+    }
+
+    @Override
+    public String toString() {
+        return "Move[" +
+                "player=" + player + ", " +
+                "lineHash=" + colorlessNormalizedLineHash + ']';
+    }
+
 }
