d28ab510adc21338406ea5f79ed5ff305d3bc033
simdeistud <simone.deiana@studenti.units.it>
2024-04-23 19:04:04 +0200

    Refactored controller to be an abstract class and contain both the view and the game.

--- src/main/java/it/units/sdm/dotsandboxes/controllers/IGameController.java
+++ src/main/java/it/units/sdm/dotsandboxes/controllers/IGameController.java
@@ -1,25 +1,65 @@
 package it.units.sdm.dotsandboxes.controllers;
 
 import it.units.sdm.dotsandboxes.core.*;
+import it.units.sdm.dotsandboxes.views.IGameView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface IGameController {
+public abstract class IGameController {
+
+    private final IGameView view;
+    private Game game;
+
+    public IGameController(final IGameView view) {
+        this.view = view;
+    }
+
 
     /**
-     * Let the UI perform an initialization step. After this method has returned successfully, the UI is
-     * assumed to be ready to render the game.
+     * Let the controller and the UI perform an initialization step. After this method has returned successfully, the UI is
+     * assumed to be ready to render the game and the game is ready to be started.
      *
      * @return whether the initialization process completed successfully
      */
-    boolean initialize();
+    public abstract boolean initialize();
+
+    /**
+     * Acquires from the user all the information necessary to start a game
+     */
+    public void setUpGame(){
+        int playerCount = getPlayerCount();
+        if(playerCount < 2){
+            throw new IllegalArgumentException("You need at least 2 players");
+        }
+        List<Player> players = new ArrayList<>(playerCount);
+        for(int playerIndex = 1; playerIndex <= playerCount; playerIndex++){
+            final Color playerColor = Color.values()[playerIndex % Color.values().length];
+            players.add(new Player(getPlayerName(playerIndex), playerColor));
+        }
+        int[] boardDimensions = getBoardDimensions();
+        game = new Game(boardDimensions[0], boardDimensions[1], players);
+    }
+
+    public void startGame(){
+        if(game == null){
+            throw new IllegalStateException("Game has not been set up!");
+        }
+        view.refresh();
+        while (!game.hasEnded()) {
+            final Line line = waitForLine(game.getCurrentPlayer());
+            makeMove(line);
+            view.refresh();
+        }
+        endGame(game.winners());
+    }
 
     /**
      * Get the number of players that will be generated and therefore will play the current game.
      *
      * @return player count [1, ]
      */
-    int getPlayerCount();
+    abstract int getPlayerCount();
 
     /**
      * Get the name to be assigned to the player with the passed number and color.
@@ -27,7 +67,7 @@ public interface IGameController {
      * @param playerNumber ordinal of the player being created
      * @return the string literal for the name to be assigned to the player being created
      */
-    String getPlayerName(int playerNumber);
+    abstract String getPlayerName(int playerNumber);
 
     /**
      * Get the number of rows and columns of the game board being created, either asking for user input or
@@ -35,14 +75,14 @@ public interface IGameController {
      *
      * @return an integer array containing two elements: int[2] { width, height }
      */
-    int[] getBoardDimensions();
+    abstract int[] getBoardDimensions();
 
     /**
      * Notify the UI to update the player visualization for the passed player instance (score counter and such).
      *
      * @param player the updated player instance
      */
-    void updatePlayer(Player player);
+    abstract void updatePlayer(Player player);
 
     /**
      * Wait for the UI to receive an event of a game turn being played by the user.
@@ -50,12 +90,26 @@ public interface IGameController {
      * @return the Line being played in the current turn by the playing Player (determined by the game instance)
      * @see Game#makeNextMove(Line)
      */
-    Line waitForLine(Player player);
+    abstract Line waitForLine(Player player);
+
+    void makeMove(Line line){
+        try {
+            game.makeNextMove(new Line(line.p1().x(), line.p1().y(), line.p2().x(), line.p2().y()));
+            game.updateScore();
+        } catch (RuntimeException e) {
+            view.drawIllegalMoveWarning(line);
+        }
+    }
 
     /**
      * Notify the UI to terminate the current game specifying the passed player as the winner.
      *
      * @param winner the winning player
      */
-    void endGame(List<Player> winner);
+    abstract void endGame(List<Player> winner);
+
+    public PostGameIntent getPostGameIntent(){
+        return view.promptForPostGameIntent();
+    }
 }
+
