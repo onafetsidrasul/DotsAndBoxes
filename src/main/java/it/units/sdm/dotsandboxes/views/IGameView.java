2986d5847565acd401b6d714b6e8ed837e6a6168
simdeistud <simone.deiana@studenti.units.it>
2024-04-23 19:09:05 +0200

    Added methods to prompt for post game intent and to display an illegal move warning

--- src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
+++ src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
@@ -1,9 +1,15 @@
 package it.units.sdm.dotsandboxes.views;
 
+import it.units.sdm.dotsandboxes.controllers.PostGameIntent;
 import it.units.sdm.dotsandboxes.core.Board;
+import it.units.sdm.dotsandboxes.core.Line;
 
 public interface IGameView {
     void init(Board gameBoard);
 
     void refresh();
+
+    PostGameIntent promptForPostGameIntent();
+
+    void displayIllegalMoveWarning(String lineString);
 }
