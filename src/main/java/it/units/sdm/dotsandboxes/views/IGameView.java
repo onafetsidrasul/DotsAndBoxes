c65a80e153e5e2c60504abf6b4c97470c39102f8
simdeistud <simone.deiana@studenti.units.it>
2024-04-24 23:50:32 +0200

    Added methods to IGameView to better model what things a view should display

--- src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
+++ src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
@@ -3,13 +3,29 @@ package it.units.sdm.dotsandboxes.views;
 import it.units.sdm.dotsandboxes.controllers.PostGameIntent;
 import it.units.sdm.dotsandboxes.core.Board;
 import it.units.sdm.dotsandboxes.core.Line;
+import it.units.sdm.dotsandboxes.core.Player;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface IGameView {
-    void init(Board gameBoard);
+    boolean init();
 
-    void refresh();
+    default void updateUI(Board gameBoard, List<Player> players, int[] scores, Player currentPlayer){};
 
-    PostGameIntent promptForPostGameIntent();
+    PostGameIntent promptForPostGameIntent() throws IOException;
 
-    void displayIllegalMoveWarning(String lineString);
+    void displayIllegalMoveWarning(Line illegalLine);
+
+    void displayIllegalActionWarning(String message);
+
+    String promptForPlayerName(int playerNumber) throws IOException;
+
+    String[] promptForBoardDimensions() throws IOException;
+
+    String promptForNumberOfPlayers() throws IOException;
+
+    String promptForMove(Player currentPlayer) throws IOException;
+
+    void displayWinners(List<Player> winners);
 }
