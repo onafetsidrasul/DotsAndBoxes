d8627cb7225dd947536a11061a7afb5d6a723be2
simdeistud <simone.deiana@studenti.units.it>
2024-04-24 23:51:49 +0200

    Added all the prompts of the view

--- src/main/java/it/units/sdm/dotsandboxes/views/ShellView.java
+++ src/main/java/it/units/sdm/dotsandboxes/views/ShellView.java
@@ -3,85 +3,209 @@ package it.units.sdm.dotsandboxes.views;
 import it.units.sdm.dotsandboxes.controllers.PostGameIntent;
 import it.units.sdm.dotsandboxes.core.Board;
 import it.units.sdm.dotsandboxes.core.Line;
+import it.units.sdm.dotsandboxes.core.Player;
+import org.fusesource.jansi.AnsiConsole;
 
-public class ShellView implements IGameView {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
-    private Board board;
+public class ShellView implements IGameView { // devo trasferire la visualizzazione dal shellgamecontroller alla shellview
 
     @Override
-    public void init(Board gameBoard) {
-        this.board=gameBoard;
+    public boolean init() {
+        AnsiConsole.systemInstall();
+        return true;
     }
 
     @Override
-    public void refresh() {
-        int width = board.length();
-        int height = board.height();
-        System.out.println(columnNumber(width));
-
-        System.out.println("  ┏" + "━".repeat(width * 4 - 1) + "┓");
-        for (int i = 0; i < height; i++) {
-            printBoardInside(i, width, height);
-        }
-        System.out.println("  ┗" + "━".repeat(width * 4 - 1) + "┛");
+    public void updateUI(final Board gameBoard, final List<Player> players, final int[] scores, final Player currentPlayer) {
+        printPlayers(players, scores);
+        printBoard(gameBoard);
+        printCurrentPlayer(currentPlayer);
     }
 
-    @Override
-    public PostGameIntent promptForPostGameIntent() {
-        return null;
+    private void printBoard(final Board gameBoard) {
+        printUpperBorder(gameBoard.length());
+        printBoardContents(gameBoard);
+        printLowerBorder(gameBoard.length());
     }
 
-    @Override
-    public void displayIllegalMoveWarning(String lineString) {
-
+    private void printUpperBorder(final int boardWidth) {
+        printColumnNumbers(boardWidth);
+        System.out.print("  ┏");
+        printHorizontalBorder(boardWidth);
+        System.out.println("┓");
     }
 
-    private void printBoardInside(int i, int width, int height) {
+    private void printColumnNumbers(int width) {
         StringBuilder sb;
-        sb = new StringBuilder(i + " ┃");
-        for (int j = 0; j < width; j++) {
-            sb.append(" ● ");
-            if (j < width - 1) { appendRowsWithHorizontalLines(j, i, sb);}
+        sb = new StringBuilder("   ");
+        for (int i = 0; i < width; i++) {
+            sb.append(" ").append(i).append("  ");
         }
-        System.out.println(sb + "┃");
-        if (i < height - 1) {
-            sb = new StringBuilder("  ┃");
-            for (int j = 0; j < width; j++) { appendRowsWithVerticalLines(j, i, sb, width);}
-            System.out.println(sb + "┃");
+        System.out.println(sb);
+    }
+
+    private void printLowerBorder(final int boardWidth) {
+        System.out.print("  ┗");
+        printHorizontalBorder(boardWidth);
+        System.out.println("┛");
+    }
+
+    private void printHorizontalBorder(final int boardWidth) {
+        System.out.print("━".repeat(boardWidth * 4 - 1));
+    }
+
+    private void printBoardContents(final Board gameBoard) {
+        for (int rowNumber = 0; rowNumber < gameBoard.height(); rowNumber++) {
+            printRowContents(rowNumber, gameBoard);
+        }
+
+    }
+
+    private void printRowContents(final int rowNumber, final Board gameBoard) {
+        StringBuilder sb;
+        sb = new StringBuilder();
+        printRowLeftBorder(rowNumber);
+        for (int j = 0; j < gameBoard.length(); j++) {
+            sb.append(" ● ");
+            if (j < gameBoard.length() - 1) {
+                printHorizontalLineIfPresent(j, rowNumber, sb, gameBoard);
+            }
+        }
+        System.out.print(sb);
+        printRowBorder();
+        System.out.println();
+        if (rowNumber < gameBoard.height() - 1) {
+            System.out.print(" ");
+            printRowBorder();
+            sb = new StringBuilder();
+            for (int j = 0; j < gameBoard.length(); j++) {
+                printVerticalLineIfPresent(j, rowNumber, sb, gameBoard);
+            }
+            System.out.print(sb);
+            printRowBorder();
+            System.out.println();
         }
     }
 
-    private void appendRowsWithVerticalLines(int j, int i, StringBuilder sb, int width) {
+    private void printRowBorder() {
+        System.out.print("┃");
+    }
+
+    private void printRowLeftBorder(final int rowNumber) {
+        System.out.print(rowNumber + " ");
+        printRowBorder();
+    }
+
+    private void printVerticalLineIfPresent(int j, int i, StringBuilder sb, Board gameBoard) {
         Line searched;
         searched = new Line(j, i, j, i + 1);
-        if (board.getLines().containsKey(searched.hashCode())) {
-            sb.append(board.getLines().get(
+        if (gameBoard.getLines().containsKey(searched.hashCode())) {
+            sb.append(gameBoard.getLines().get(
                     searched.hashCode()).color().getFormat().format(" ‖ "));
         } else {
             sb.append("   ");
         }
-        if (j < width - 1) {
+        if (j < gameBoard.length() - 1) {
             sb.append(" ");
         }
     }
 
-    private void appendRowsWithHorizontalLines(int j, int i, StringBuilder sb) {
+    private void printHorizontalLineIfPresent(int j, int i, StringBuilder sb, Board gameBoard) {
         Line searched;
         searched = new Line(j, i, j + 1, i);
-        if (board.getLines().containsKey(searched.hashCode())) {
-            sb.append(board.getLines().get(
+        if (gameBoard.getLines().containsKey(searched.hashCode())) {
+            sb.append(gameBoard.getLines().get(
                     searched.hashCode()).color().getFormat().format("="));
         } else {
             sb.append(" ");
         }
     }
 
-    private StringBuilder columnNumber(int width) {
-        StringBuilder sb;
-        sb = new StringBuilder("   ");
-        for (int j = 0; j < width; j++) {
-            sb.append(" ").append(j).append("  ");
+    private void printPlayers(List<Player> players, int[] scores) {
+        System.out.println("--- PLAYERS ---");
+        for (int i = 1; i <= players.size(); i++) {
+            Player player = players.get(i - 1);
+            System.out.println("Player " + i + " : " + player.name() + ", " + player.color());
+            System.out.println("\tScore: " + scores[i - 1]);
         }
-        return sb;
+        System.out.println("---------------");
     }
+
+    private void printCurrentPlayer(Player currentPlayer) {
+        System.out.println("Current player: " + currentPlayer.name());
+    }
+
+    @Override
+    public PostGameIntent promptForPostGameIntent() throws IOException {
+        System.out.print("Do you wish to continue? [y/n] : ");
+        String intent = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        intent = reader.readLine();
+        return intent.equals("y") ? PostGameIntent.NEW_GAME : PostGameIntent.END_GAME;
+    }
+
+    @Override
+    public void displayIllegalMoveWarning(Line illegalLine) {
+        System.out.println("Line " + illegalLine + " is not allowed, try again");
+    }
+
+    @Override
+    public void displayIllegalActionWarning(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public String promptForPlayerName(int playerNumber) throws IOException {
+        System.out.print("Insert name for player" + playerNumber + " : ");
+        String name = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        name = reader.readLine();
+        return name;
+    }
+
+    @Override
+    public String[] promptForBoardDimensions() throws IOException {
+        System.out.print("Insert board dimensions [ NxM ] : ");
+        String dimensions = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        dimensions = reader.readLine();
+        return dimensions.split("x");
+    }
+
+    @Override
+    public String promptForNumberOfPlayers() throws IOException {
+        System.out.print("Insert number of players : ");
+        String playerNum = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        playerNum = reader.readLine();
+        return playerNum;
+    }
+
+    @Override
+    public String promptForMove(Player currentPlayer) throws IOException {
+        System.out.print("Make your move [ x1, y1, x2, y2 ] : ");
+        String move = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        move = reader.readLine();
+        return move;
+    }
+
+    @Override
+    public void displayWinners(List<Player> winners) {
+        if (winners.size() > 1) {
+            System.out.println("Game tied between the players: ");
+            for (Player winner : winners) {
+                System.out.println(winner.name());
+            }
+        }
+        if (winners.size() == 1) {
+            System.out.println("Player " + winners.getFirst().name() + " won!");
+        }
+    }
+
+
 }
