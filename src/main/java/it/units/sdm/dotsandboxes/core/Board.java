75afbf92da1cb2dd9762a17437a4feb3fb67e7ee
simdeistud <simone.deiana@studenti.units.it>
2024-07-02 19:40:39 +0200

    Changed data structure for the placed lines. Refactored following the changes to Line.

--- src/main/java/it/units/sdm/dotsandboxes/core/Board.java
+++ src/main/java/it/units/sdm/dotsandboxes/core/Board.java
@@ -1,64 +1,80 @@
 package it.units.sdm.dotsandboxes.core;
 
-import java.util.HashMap;
-import java.util.Map;
+import it.units.sdm.dotsandboxes.exceptions.InvalidInputException;
+
+import java.util.*;
 
 public class Board {
-    private final Map<Integer, Line> lines;
-    private final int length, height;
+    private final SequencedCollection<ColoredLine> lines;
+    private final int width, height;
 
-    public Board(int xDimension, int yDimension) {
-        length = xDimension;
-        height = yDimension;
-        lines = new HashMap<>();
+    public Board(int width, int height) {
+        if (width < 2 || height < 2) {
+            throw new IllegalArgumentException("Board width and height must be greater than 2");
+        }
+        this.width = width;
+        this.height = height;
+        lines = new ArrayList<>();
     }
 
+    /**
+     * @param p The upper left point of the box to check
+     * @return false if the box is not completed, true otherwise.
+     */
     public boolean isBoxCompleted(Point p) {
-        //box identified by the upper left point
-        Integer upperSideHash = new Line(p.x(), p.y(), p.x() + 1, p.y()).hashCode();
-        Integer lowerSideHash = new Line(p.x(), p.y() + 1, p.x() + 1, p.y() + 1).hashCode();
-        Integer leftSideHash = new Line(p.x(), p.y(), p.x(), p.y() + 1).hashCode();
-        Integer rightSideHash = new Line(p.x() + 1, p.y(), p.x() + 1, p.y() + 1).hashCode();
-
-        return lines.containsKey(upperSideHash) &&
-                lines.containsKey(lowerSideHash) &&
-                lines.containsKey(leftSideHash) &&
-                lines.containsKey(rightSideHash);
+        if (p.x() < 0 || p.x() >= width || p.y() < 0 || p.y() >= height) {
+            throw new IllegalArgumentException("Point does not represent a square on the board");
+        }
+        return lineSitsBetween(p, new Point(p.x(), p.y() + 1)) &&
+                lineSitsBetween(p, new Point(p.x() + 1, p.y())) &&
+                lineSitsBetween(new Point(p.x() + 1, p.y() + 1), new Point(p.x(), p.y() + 1)) &&
+                lineSitsBetween(new Point(p.x() + 1, p.y() + 1), new Point(p.x() + 1, p.y()));
     }
 
     public boolean isBoardFull() {
         // since we check the validity of every line drawn onto the board we can check if the board has been completely filled
         // (i.e. the games has ended) by simply checking if the number of lines is 2*n*m - n - m which is the amount of possible lines
         // for a n*m board
-        return lines.size() == (2 * length * height) - length - height;
+        return lines.size() == (2 * width * height) - width - height;
     }
 
-    protected void addLine(Line line) {
+    protected void placeLine(ColoredLine line) throws InvalidInputException {
         if (line.length() != 1)
-            throw new IllegalArgumentException("Line is too long!");
+            throw new InvalidInputException("Line is too long.");
         if (isLineOutOfBounds(line))
-            throw new IllegalArgumentException("Line sits outside the bounds of the board!");
-        if (lines.containsKey(new Line(null, Line.normalize(line)).hashCode())) {
-            throw new IllegalArgumentException("Line already exists!");
+            throw new InvalidInputException("Line sits outside the bounds of the board.");
+        if (lines.parallelStream().anyMatch(l -> l.hasSameEndpointsAs(line))) {
+            throw new InvalidInputException("A line already exists between endpoints " + line.p1() + " and " + line.p2());
         }
-        // the hashcode is calculated based on the line stripped of its color and normalized in order to avoid putting to lines of different colors in the same place
-        lines.put(new Line(null, Line.normalize(line)).hashCode(), line);
+        lines.add(line);
     }
 
     private boolean isLineOutOfBounds(Line line) {
-        return line.p1().x() < 0 || line.p1().x() >= length || line.p1().y() < 0 || line.p1().y() >= height ||
-                line.p2().x() < 0 || line.p2().x() >= length || line.p2().y() < 0 || line.p2().y() >= height;
+        return line.p1().x() < 0 || line.p1().x() >= width ||
+                line.p1().y() < 0 || line.p1().y() >= height ||
+                line.p2().x() < 0 || line.p2().x() >= width ||
+                line.p2().y() < 0 || line.p2().y() >= height;
+    }
+
+    private boolean lineSitsBetween(Point p1, Point p2) {
+        return lines.parallelStream().anyMatch(l -> {
+            try {
+                return l.hasSameEndpointsAs(new Line(p1, p2));
+            } catch (InvalidInputException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public int length() {
-        return length;
+        return width;
     }
 
     public int height() {
         return height;
     }
 
-    public Map<Integer, Line> getLines() {
+    public SequencedCollection<ColoredLine> lines() {
         return lines;
     }
 }
