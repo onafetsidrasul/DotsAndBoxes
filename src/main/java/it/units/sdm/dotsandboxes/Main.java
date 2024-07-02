40a64d85ff21154efb5ce261b1e2be6e7b9eff04
simdeistud <simone.deiana@studenti.units.it>
2024-07-02 10:13:31 +0200

    Added exceptions handling

--- src/main/java/it/units/sdm/dotsandboxes/Main.java
+++ src/main/java/it/units/sdm/dotsandboxes/Main.java
@@ -7,23 +7,25 @@ import it.units.sdm.dotsandboxes.views.ShellView;
 
 public class Main {
 
-  private static final IGameSaver<GameSession> saver = new JsonGameSaver();
+    private static final IGameSaver<GameSession> saver = new JsonGameSaver();
 
-  public static void main(String... args) {
-    if (args.length == 1) {
-      final GameSession session = switch (args[0]) {
-        case "tui" -> new GameSession(new ShellGameController(new ShellView()));
-        //case "gui" -> new GameSession(new SwingGameController(new SwingView()));
-        default -> (GameSession) saver.restoreFromFile(args[0]);
-      };
-      try {
-        session.start();
-      } finally {
-        System.out.println("Game has terminated.");
-      }
-    } else {
-      System.out.println("Usage: \n java -jar DotsAndBoxes.jar <gamemode> \n java -jar DotsAndBoxes.jar <gamesave>");
+    public static void main(String... args) {
+        if (args.length == 1) {
+            final GameSession session = switch (args[0]) {
+                case "tui" -> new GameSession(new ShellGameController(new ShellView()));
+                //case "gui" -> new GameSession(new SwingGameController(new SwingView()));
+                default -> (GameSession) saver.restoreFromFile(args[0]);
+            };
+            try {
+                session.start();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.println("Game has terminated.");
+            }
+        } else {
+            System.out.println("Usage: \n java -jar DotsAndBoxes.jar <gamemode> \n java -jar DotsAndBoxes.jar <gamesave>");
+        }
+
     }
-
-  }
 }
