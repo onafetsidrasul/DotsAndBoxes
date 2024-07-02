ad4cf6026486baffdf1305b40660111a4892f2d8
simdeistud <simone.deiana@studenti.units.it>
2024-07-02 07:17:18 +0200

    Refactored to clear control flow

--- src/main/java/it/units/sdm/dotsandboxes/Main.java
+++ src/main/java/it/units/sdm/dotsandboxes/Main.java
@@ -1,33 +1,29 @@
 package it.units.sdm.dotsandboxes;
 
 import it.units.sdm.dotsandboxes.controllers.ShellGameController;
-import it.units.sdm.dotsandboxes.exceptions.UserHasRequestedQuit;
 import it.units.sdm.dotsandboxes.persistence.IGameSaver;
 import it.units.sdm.dotsandboxes.persistence.JsonGameSaver;
 import it.units.sdm.dotsandboxes.views.ShellView;
 
-import java.util.Arrays;
-import java.util.Date;
-
 public class Main {
 
-    private static final IGameSaver<GameSession> saver = new JsonGameSaver();
+  private static final IGameSaver<GameSession> saver = new JsonGameSaver();
 
-    public static void main(String[] args) {
-        final GameSession session;
-        if (args.length > 0) {
-            session = (GameSession) saver.restoreFromFile(args[0]);
-        } else {
-            session = new GameSession(new ShellGameController(new ShellView()));
-        }
-        try {
-            session.start();
-        } catch (UserHasRequestedQuit e) {
-            final String filename;
-            if ((filename = saver.save(session)) != null) {
-                System.out.println("Saved game session in " + filename);
-            }
-            System.out.println("Bye!");
-        }
+  public static void main(String... args) {
+    if (args.length == 1) {
+      final GameSession session = switch (args[0]) {
+        case "tui" -> new GameSession(new ShellGameController(new ShellView()));
+        //case "gui" -> new GameSession(new SwingGameController(new SwingView()));
+        default -> (GameSession) saver.restoreFromFile(args[0]);
+      };
+      try {
+        session.start();
+      } finally {
+        System.out.println("Game has terminated.");
+      }
+    } else {
+      System.out.println("Usage: \n java -jar DotsAndBoxes.jar <gamemode> \n java -jar DotsAndBoxes.jar <gamesave>");
     }
+
+  }
 }
