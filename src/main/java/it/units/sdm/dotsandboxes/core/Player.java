e7dcc08e09260c2fdb5ef8090a4870ad8af75f2a
simdeistud <simone.deiana@studenti.units.it>
2024-07-02 10:38:58 +0200

    Turned Player into a record

--- src/main/java/it/units/sdm/dotsandboxes/core/Player.java
+++ src/main/java/it/units/sdm/dotsandboxes/core/Player.java
@@ -3,56 +3,16 @@ package it.units.sdm.dotsandboxes.core;
 import java.util.Objects;
 import java.util.UUID;
 
-public class Player {
-
-    private final String id;
-    private final String name;
-    private final Color color;
-
-    public Player(String name, Color color) {
-        this(UUID.randomUUID().toString(), name, color);
-    }
-
-    public Player(String id, String name, Color color) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public Color color() {
-        return color;
-    }
-
-    public String id() {
-        return id;
-    }
-
+public record Player(String name, Color color){
     @Override
-    public final boolean equals(Object o) {
+    public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Player player)) return false;
-
-        return Objects.equals(id, player.id) && Objects.equals(name, player.name) && color == player.color;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hashCode(id);
-        result = 31 * result + Objects.hashCode(name);
-        result = 31 * result + Objects.hashCode(color);
-        return result;
+        if (o == null || getClass() != o.getClass()) return false;
+        return Objects.equals(name, ((Player) o).name);
     }
 
     @Override
     public String toString() {
-        return "Player{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", color=" + color +
-                '}';
+        return name;
     }
 }
