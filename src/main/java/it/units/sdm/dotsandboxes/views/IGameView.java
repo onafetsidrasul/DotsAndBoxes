9c9aac6aaaf3d3cc27ad1cf9205fc9cc82cb9b56
simdeistud <simone.deiana@studenti.units.it>
2024-07-06 11:23:45 +0200

    Turned the IGameView into an abstract class. Adapted it to fit the async paradigm and made the methods return values through String. Added JavaDoc

--- src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
+++ src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
@@ -1,32 +1,74 @@
 package it.units.sdm.dotsandboxes.views;
 
-import it.units.sdm.dotsandboxes.core.Board;
-import it.units.sdm.dotsandboxes.core.Color;
+import it.units.sdm.dotsandboxes.controllers.IGameController;
+import it.units.sdm.dotsandboxes.core.Game;
 
+public abstract class IGameView implements Runnable {
 
-import java.util.Map;
-import java.util.SequencedCollection;
+    protected IGameController controllerReference;
+    protected Game gameStateReference;
+    protected boolean isInitialized = false;
+    protected boolean isConfigured = false;
 
-public interface IGameView {
-    boolean init();
+    /** Initializes the view.
+     * @return true if initialization was successful, false otherwise.
+     */
+    public abstract boolean init(final IGameController controllerReference);
 
-    default void updateUI(Board gameBoard, SequencedCollection<String> players, Map<String, Integer> scores, Map<String, Color> colors, String currentPlayer){}
+    /** Configures the state of the game into the view. View must have been already initialized.
+     * @return true if the configuration was successful, false otherwise.
+     */
+    public abstract boolean configure(final Game gameStateReference);
 
-    void promptForPostGameIntent();
+    /**
+     * Starts the display loop of the game. View must be initialized and configured.
+     */
+    public abstract void startGameUI();
 
-    void displayMessage(String message);
+    /** Prompts the user to input the gamemode.
+     * @return "PVP" : Against players
+     *          "PVE" : Against the computer
+     */
+    public abstract String promptForGamemode();
 
-    void displayIllegalActionWarning(String message);
+    public abstract String promptForNumberOfPlayers();
 
-    void promptForPlayerName(int playerNumber);
+    public String[] promptForBoardDimensions(){
+        return new String[]{promptForBoardHeight(), promptForBoardWidth()};
+    }
 
-    void promptForBoardDimensions();
+    public abstract String promptForBoardHeight();
 
-    void promptForNumberOfPlayers();
+    public abstract String promptForBoardWidth();
 
-    void promptForAction(String currentPlayer);
+    public String[] promptForPlayersNames(int numOfPlayers){
+        String[] names = new String[numOfPlayers];
+        for (int i = 0; i < numOfPlayers; i++) {
+            names[i] = promptForPlayerName(i);
+        }
+        return names;
+    }
 
-    void displayWinners(SequencedCollection<String> winners);
+    public abstract String promptForPlayerName(int playerNumber);
 
-    void promptForGamemode();
+    /** Prompts the user regarding what to do at the end of the game.
+     * @return "NEW" : New game
+     *          "END" : End game
+     */
+    public abstract String promptForPostGameIntent();
+
+    /** Displays a message to the user.
+     * @param message Text to display.
+     */
+    public abstract void displayMessage(String message);
+
+    /** Displays a blocking warning to the user.
+     * @param message Text to display.
+     */
+    public abstract void displayWarning(String message);
+
+    /** Displays the winner(s) of the game.
+     */
+    public abstract void displayResults();
+
 }
