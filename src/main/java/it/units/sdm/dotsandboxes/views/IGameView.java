a9f2d2e28fdf44934f2ad6d73316becc12df5fba
simdeistud <simone.deiana@studenti.units.it>
2024-07-06 14:54:32 +0200

    Refactoring for better control of operations

--- src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
+++ src/main/java/it/units/sdm/dotsandboxes/views/IGameView.java
@@ -3,6 +3,9 @@ package it.units.sdm.dotsandboxes.views;
 import it.units.sdm.dotsandboxes.controllers.IGameController;
 import it.units.sdm.dotsandboxes.core.Game;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public abstract class IGameView implements Runnable {
 
     protected IGameController controllerReference;
@@ -10,30 +13,77 @@ public abstract class IGameView implements Runnable {
     protected boolean isInitialized = false;
     protected boolean isConfigured = false;
 
-    /** Initializes the view.
+    /**
+     * Initializes the view.
+     *
      * @return true if initialization was successful, false otherwise.
      */
-    public abstract boolean init(final IGameController controllerReference);
+    public final boolean init(final IGameController controllerReference) {
+        isInitialized = assignControllerReference(controllerReference) && finishInit();
+        return isInitialized;
+    }
 
-    /** Configures the state of the game into the view. View must have been already initialized.
+    protected abstract boolean finishInit();
+
+    private boolean assignControllerReference(final IGameController controllerReference) {
+        try {
+            Objects.requireNonNull(controllerReference);
+            this.controllerReference = controllerReference;
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Configures the state of the game into the view. View must have been already initialized.
+     *
      * @return true if the configuration was successful, false otherwise.
      */
-    public abstract boolean configure(final Game gameStateReference);
+    public final boolean configure(final Game gameStateReference) {
+        isConfigured = isInitialized && assignGameStateReference(gameStateReference) && finishConfigure();
+        return isConfigured;
+    }
+
+    private boolean assignGameStateReference(final Game gameStateReference) {
+        try {
+            Objects.requireNonNull(gameStateReference);
+            this.gameStateReference = gameStateReference;
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    protected abstract boolean finishConfigure();
 
     /**
      * Starts the display loop of the game. View must be initialized and configured.
      */
-    public abstract void startGameUI();
+    public void startGameUI() throws IOException {
+        if (!isInitializedAndConfigured()) {
+            throw new IOException("View was not initialized and configured properly");
+        }
+        displayGameUI();
+    }
 
-    /** Prompts the user to input the gamemode.
+    protected boolean isInitializedAndConfigured() {
+        return isInitialized && isConfigured;
+    }
+
+    protected abstract void displayGameUI();
+
+    /**
+     * Prompts the user to input the gamemode.
+     *
      * @return "PVP" : Against players
-     *          "PVE" : Against the computer
+     * "PVE" : Against the computer
      */
     public abstract String promptForGamemode();
 
     public abstract String promptForNumberOfPlayers();
 
-    public String[] promptForBoardDimensions(){
+    public String[] promptForBoardDimensions() {
         return new String[]{promptForBoardHeight(), promptForBoardWidth()};
     }
 
@@ -41,7 +91,7 @@ public abstract class IGameView implements Runnable {
 
     public abstract String promptForBoardWidth();
 
-    public String[] promptForPlayersNames(int numOfPlayers){
+    public String[] promptForPlayersNames(int numOfPlayers) {
         String[] names = new String[numOfPlayers];
         for (int i = 0; i < numOfPlayers; i++) {
             names[i] = promptForPlayerName(i);
@@ -51,23 +101,30 @@ public abstract class IGameView implements Runnable {
 
     public abstract String promptForPlayerName(int playerNumber);
 
-    /** Prompts the user regarding what to do at the end of the game.
+    /**
+     * Prompts the user regarding what to do at the end of the game.
+     *
      * @return "NEW" : New game
-     *          "END" : End game
+     * "END" : End game
      */
     public abstract String promptForPostGameIntent();
 
-    /** Displays a message to the user.
+    /**
+     * Displays a message to the user.
+     *
      * @param message Text to display.
      */
     public abstract void displayMessage(String message);
 
-    /** Displays a blocking warning to the user.
+    /**
+     * Displays a blocking warning to the user.
+     *
      * @param message Text to display.
      */
     public abstract void displayWarning(String message);
 
-    /** Displays the winner(s) of the game.
+    /**
+     * Displays the winner(s) of the game.
      */
     public abstract void displayResults();
 
