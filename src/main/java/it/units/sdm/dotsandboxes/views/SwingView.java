04fcbc8132079dcd99480a51ec7bfeee6bb95779
onafetsidrasul <110050435+onafetsidrasul@users.noreply.github.com>
2024-08-13 22:15:50 +0200

    Moved BoardPanel and ScorePanel to SwingView, refactored BoardPanel

--- src/main/java/it/units/sdm/dotsandboxes/views/SwingView.java
+++ src/main/java/it/units/sdm/dotsandboxes/views/SwingView.java
@@ -1,8 +1,22 @@
 package it.units.sdm.dotsandboxes.views;
 
+import it.units.sdm.dotsandboxes.controllers.IGameController;
+import it.units.sdm.dotsandboxes.core.ColoredLine;
+import it.units.sdm.dotsandboxes.core.Game;
+import it.units.sdm.dotsandboxes.core.Line;
+import it.units.sdm.dotsandboxes.core.Point;
+import it.units.sdm.dotsandboxes.exceptions.InvalidInputException;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class SwingView extends IGameView implements Runnable {
 
@@ -173,11 +187,223 @@ public class SwingView extends IGameView implements Runnable {
     private void refreshBoardComponents() {
         synchronized (gameStateReference.board) {
             boardPanel.repaint();
-            mainPanel.revalidate();
-            mainPanel.repaint();
             scorePanel.updateTurn(gameStateReference);
             scorePanel.updateScore(gameStateReference);
         }
         isRefreshingUISem.release();
+    }
+
+    public static class BoardPanel extends JPanel {
+        private static final int PADDING = 20;
+        private static final int DOT_SIZE = 20;
+        private static final int LINE_THICKNESS = 2;
+
+        private final List<JRadioButton> selectedButtons = new ArrayList<>();
+        private final Game gameState;
+        private final IGameController controller;
+
+        private Point firstPoint;
+        private boolean isSelectingFirstPoint = true;
+
+        public BoardPanel(Game gameState, IGameController controller) {
+            this.gameState = gameState;
+            this.controller = controller;
+            setLayout(null);
+            initializeDotButtons();
+            addResizeListener();
+        }
+
+        private void initializeDotButtons() {
+            int width = gameState.board.width();
+            int height = gameState.board.height();
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    add(createDotButton(x, y));
+                }
+            }
+            updateDotButtonPositions();
+        }
+
+        private JRadioButton createDotButton(int x, int y) {
+            JRadioButton dotButton = new JRadioButton();
+            dotButton.setPreferredSize(new Dimension(DOT_SIZE, DOT_SIZE));
+            dotButton.setActionCommand(x + " " + y);
+            dotButton.addActionListener(new DotButtonActionListener());
+            return dotButton;
+        }
+
+        private void addResizeListener() {
+            addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    updateDotButtonPositions();
+                    repaint();
+                }
+            });
+        }
+
+        private void updateDotButtonPositions() {
+            int width = gameState.board.width();
+            int height = gameState.board.height();
+
+            int cellWidth = calculateCellDimension(getWidth(), width);
+            int cellHeight = calculateCellDimension(getHeight(), height);
+
+            for (Component component : getComponents()) {
+                if (component instanceof JRadioButton) {
+                    updateButtonPosition((JRadioButton) component, cellWidth, cellHeight);
+                }
+            }
+        }
+
+        private void updateButtonPosition(JRadioButton button, int cellWidth, int cellHeight) {
+            String[] coordinates = button.getActionCommand().split(" ");
+            int x = Integer.parseInt(coordinates[0]);
+            int y = Integer.parseInt(coordinates[1]);
+            int dotX = PADDING + x * cellWidth;
+            int dotY = PADDING + y * cellHeight;
+            button.setBounds(dotX - DOT_SIZE / 2, dotY - DOT_SIZE / 2, DOT_SIZE, DOT_SIZE);
+        }
+
+        private int calculateCellDimension(int totalSize, int count) {
+            return (totalSize - 2 * PADDING) / (count - 1);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            int width = gameState.board.width();
+            int height = gameState.board.height();
+
+            int cellWidth = calculateCellDimension(getWidth(), width);
+            int cellHeight = calculateCellDimension(getHeight(), height);
+
+            synchronized (gameState.board.lines()) {
+                drawLines(g2, cellWidth, cellHeight, true);
+                drawLines(g2, cellWidth, cellHeight, false);
+            }
+        }
+
+        private void drawLines(Graphics2D g2, int cellWidth, int cellHeight, boolean isHorizontal) {
+            int width = gameState.board.width();
+            int height = gameState.board.height();
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    drawLineIfPresent(g2, x, y, cellWidth, cellHeight, isHorizontal);
+                }
+            }
+        }
+
+        private void drawLineIfPresent(Graphics2D g2, int x, int y, int cellWidth, int cellHeight, boolean isHorizontal) {
+            Line line = createLine(x, y, isHorizontal);
+
+            Optional<ColoredLine> lineOpt = gameState.board.lines().stream()
+                    .filter(coloredLine -> coloredLine.hasSameEndpointsAs(line))
+                    .findFirst();
+
+            lineOpt.ifPresent(coloredLine -> drawLine(g2, coloredLine, x, y, cellWidth, cellHeight, isHorizontal));
+        }
+
+        private Line createLine(int x, int y, boolean isHorizontal) {
+            try {
+                return isHorizontal ? new Line(x, y, x + 1, y) : new Line(x, y, x, y + 1);
+            } catch (InvalidInputException e) {
+                throw new RuntimeException("Invalid line coordinates", e);
+            }
+        }
+
+        private void drawLine(Graphics2D g2, ColoredLine coloredLine, int x, int y, int cellWidth, int cellHeight, boolean isHorizontal) {
+            g2.setColor(coloredLine.color().toAwtColor());
+            int dotX = PADDING + x * cellWidth;
+            int dotY = PADDING + y * cellHeight;
+
+            if (isHorizontal) {
+                g2.fillRect(dotX, dotY - LINE_THICKNESS / 2, cellWidth, LINE_THICKNESS);
+            } else {
+                g2.fillRect(dotX - LINE_THICKNESS / 2, dotY, LINE_THICKNESS, cellHeight);
+            }
+        }
+
+        private class DotButtonActionListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Point clickedPoint = parseCoordinates(e.getActionCommand());
+                handleDotSelection(clickedPoint, (JRadioButton) e.getSource());
+            }
+
+            private Point parseCoordinates(String actionCommand) {
+                String[] coords = actionCommand.split(" ");
+                return new Point(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+            }
+
+            private void handleDotSelection(Point clickedPoint, JRadioButton sourceButton) {
+                selectedButtons.add(sourceButton);
+
+                if (isSelectingFirstPoint) {
+                    firstPoint = clickedPoint;
+                    isSelectingFirstPoint = false;
+                } else {
+                    if (firstPoint != null) {
+                        processLineSelection(clickedPoint);
+                        resetSelection();
+                    }
+                }
+            }
+
+            private void processLineSelection(Point secondPoint) {
+                controller.input = formatLineInput(firstPoint, secondPoint);
+                controller.inputHasBeenReceivedSem.release();
+            }
+
+            private String formatLineInput(Point first, Point second) {
+                return first.x() + " " + first.y() + " " + second.x() + " " + second.y();
+            }
+
+            private void resetSelection() {
+                firstPoint = null;
+                isSelectingFirstPoint = true;
+
+                if (selectedButtons.size() == 2) {
+                    selectedButtons.forEach(button -> button.setSelected(false));
+                    selectedButtons.clear();
+                }
+            }
+        }
+    }
+
+    private static class ScorePanel extends JPanel {
+        private final JLabel scoreLabels;
+        private final JLabel turnLabel;
+
+        public ScorePanel() {
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            scoreLabels = new JLabel();
+            turnLabel = new JLabel("Turn: Player 1");
+            add(scoreLabels);
+            add(turnLabel);
+        }
+
+        public void updateScore(Game game) {
+            StringBuilder scores = new StringBuilder("<html>");
+            for (int i = 1; i <= game.players.size(); i++) {
+                String player = game.players.get(i - 1);
+                String score = String.valueOf(game.scoreBoard.get(player));
+                scores.append(player).append(": ").append(score).append("<br>");
+            }
+            scores.append("</html>");
+            scoreLabels.setText(scores.toString());
+        }
+
+        public void updateTurn(Game game) {
+            String currentPlayer = game.currentPlayer();
+            turnLabel.setText("Current player: " + currentPlayer);
+            Color playerColor = game.playerColorLUT.get(currentPlayer).toAwtColor();
+            turnLabel.setForeground(playerColor);
+        }
     }
 }
