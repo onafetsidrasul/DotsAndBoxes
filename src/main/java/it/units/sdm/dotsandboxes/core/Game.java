b70871915e8df22cdedb8be38872a5133228fd33
onafetsidrasul <110050435+onafetsidrasul@users.noreply.github.com>
2024-08-19 17:39:14 +0200

    Update Game.java
    Resolved a problem of merge

--- src/main/java/it/units/sdm/dotsandboxes/core/Game.java
+++ src/main/java/it/units/sdm/dotsandboxes/core/Game.java
@@ -62,11 +62,6 @@ public class Game implements Savable<Game> {
             }
             return (board.lines().size() - 1) % players.size();
         }
-        synchronized (board.lines()){
-        if (board.lines().isEmpty()) {
-            return -1;
-        }
-        return (board.lines().size() - 1) % players.size();}
     }
 
     public int getCurrentPlayerIndex() {
@@ -75,7 +70,7 @@ public class Game implements Savable<Game> {
 
     public String currentPlayer() {
         // we chose to make the player1 start first every time
-        synchronized(board.lines()){
+        synchronized (board.lines()) {
             if (board.lines().isEmpty())
                 return this.players.getFirst();
             return this.players.get(getCurrentPlayerIndex());
@@ -85,7 +80,7 @@ public class Game implements Savable<Game> {
 
     public String getLastPlayer() {
         // we chose to make the player1 start first every time
-        synchronized (board.lines()){
+        synchronized (board.lines()) {
             if (board.lines().isEmpty())
                 return null;
             return this.players.get(getLastPlayerIndex());
@@ -102,11 +97,11 @@ public class Game implements Savable<Game> {
     }
 
     public void makeNextMove(Line line) throws InvalidInputException {
-        ColoredLine lineCandidate =  line == null ? null : new ColoredLine(line, getPlayerColor(currentPlayer()));
+        ColoredLine lineCandidate = line == null ? null : new ColoredLine(line, getPlayerColor(currentPlayer()));
         board.placeLine(lineCandidate);
     }
 
-    public Board board() {
+    public final Board board() {
         return board;
     }
 
